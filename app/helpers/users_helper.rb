3e88cb840da28e0b2db17fb15a0af33c971f33c5
A Machalica <a.machalica@selleo.com>
2016-01-11 12:24:11 +0100

    Sorting users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+
+  def id_sort
+    id_order = params.fetch(:order, {}).fetch(:id, {})
+    return ['asc', {}].include?(id_order) ? 'desc' : 'asc'
+  end
+
+  def first_name_sort
+    first_name_order = params.fetch(:order, {}).fetch(:first_name, {})
+    return ['asc', {}].include?(first_name_order) ? 'desc' : 'asc'
+  end
+
+  def last_name_sort
+    last_name_order = params.fetch(:order, {}).fetch(:last_name, {})
+    return ['asc', {}].include?(last_name_order) ? 'desc' : 'asc'
+  end
+
+  def email_sort
+    email_order = params.fetch(:order, {}).fetch(:email, {})
+    return ['asc', {}].include?(email_order) ? 'desc' : 'asc'
+  end
+
 end
