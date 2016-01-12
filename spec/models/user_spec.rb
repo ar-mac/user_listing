dadf492c99204f9a513d876871e50656928badb8
A Machalica <a.machalica@selleo.com>
2016-01-12 13:32:09 +0100

    user methods spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,20 @@ RSpec.describe User do
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_length_of(:last_name).is_at_least(2).is_at_most(20) }
   it { is_expected.to validate_presence_of(:email) }
-  # it { is_expected.to validate_length_of(:email).is_at_most(20) }
   it { is_expected.to allow_values('user1@example.com', '123guy_@company.eu').for(:email) }
   it { is_expected.not_to allow_values('user1example.com', '123guy_@company_eu').for(:email) }
 
+  it { is_expected.to have_and_belong_to_many(:projects) }
+
+  describe '#full_name' do
+    let(:user) { build(:user) }
+
+    it { expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")}
+  end
+
+  describe '#to_s' do
+    let(:user) { build(:user) }
+
+    it { expect(user.to_s).to eq(user.full_name)}
+  end
 end
