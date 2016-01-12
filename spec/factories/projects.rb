601741adf6b3d4c8ad6273cea468bfd5e28294f6
A Machalica <a.machalica@selleo.com>
2016-01-12 10:56:34 +0100

    trait for inactive project

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,11 +1,16 @@
 FactoryGirl.define do
   factory :project do
     sequence(:name) { |n| "project ##{n}" }
+    active true
 
     trait :with_users do
       after(:create) do |project|
         create_list(:user, 2, project_ids: project.id)
       end
+    end
+
+    trait :inactive do
+      active false
     end
   end
 
