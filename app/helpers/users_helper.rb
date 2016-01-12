1b045f00eb03b9f73ae413e9943f2be3c2eaf9b4
A Machalica <a.machalica@selleo.com>
2016-01-12 09:02:11 +0100

    users helper refactor

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,9 @@
 module UsersHelper
 
-  def first_name_sort
-    first_name_order = params.fetch(:order, {}).fetch(:first_name, {})
-    return ['asc', {}].include?(first_name_order) ? 'desc' : 'asc'
+  def params_for_order_column(column)
+    column = column.to_sym
+    current_ordering = params.fetch(:order, {}).fetch(column, {})
+    new_ordering = ['asc', {}].include?(current_ordering) ? 'desc' : 'asc'
+    return { order: {column => new_ordering} }
   end
-
-  def last_name_sort
-    last_name_order = params.fetch(:order, {}).fetch(:last_name, {})
-    return ['asc', {}].include?(last_name_order) ? 'desc' : 'asc'
-  end
-
-  def email_sort
-    email_order = params.fetch(:order, {}).fetch(:email, {})
-    return ['asc', {}].include?(email_order) ? 'desc' : 'asc'
-  end
-
 end
