6b7dfcc1add42beebdc54cb38c1749e9b36347d4
A Machalica <a.machalica@selleo.com>
2016-01-12 11:05:53 +0100

    user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_length_of(:first_name).is_at_least(2).is_at_most(20) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_length_of(:last_name).is_at_least(2).is_at_most(20) }
+  it { is_expected.to validate_presence_of(:email) }
+  # it { is_expected.to validate_length_of(:email).is_at_most(20) }
+  it { is_expected.to allow_values('user1@example.com', '123guy_@company.eu').for(:email) }
+  it { is_expected.not_to allow_values('user1example.com', '123guy_@company_eu').for(:email) }
+
 end
