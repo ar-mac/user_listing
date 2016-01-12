bef71e8eb62ea271f6d52a458b29cfe5fcbe7bbc
A Machalica <a.machalica@selleo.com>
2016-01-12 10:16:21 +0100

    rename method params_for_order_column

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-  def params_for_order_column(column)
+  def params_for_column_ordering(column)
     column = column.to_sym
     current_ordering = params.fetch(:order, {}).fetch(column, {})
     new_ordering = ['asc', {}].include?(current_ordering) ? 'desc' : 'asc'
